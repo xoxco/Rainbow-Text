20d21627fcf030504a2cb713f316e405905308bb
Ben Brown <ben@xoxco.com>
2011-10-05 12:15:19 -0500

    First commit

---
+++ rainbow.js
@@ -0,0 +1,99 @@
+(function($) {
+	$.fn.rainbow = function(options) {
+		this.each(function() {
+		
+			options.originalText = $(this).html();
+			options.iterations = 0;
+			if (!options.pauseLength) {
+				options.pauseLength = options.animateInterval;
+			}
+			$(this).data('options',options);
+			
+			
+			if (options.pad) {
+			
+				for (x = 0; x < options.originalText.length; x++) {
+					options.colors.unshift(options.colors[options.colors.length-1]);
+				}
+			}
+			
+			$.fn.rainbow.render(this);		
+			
+		});
+	}
+	
+	$.fn.rainbowStop = function() {
+	
+		this.each(function() {
+			var options = $(this).data('option');
+			if (options.interval) {
+				clearInterval(options.interval);
+				options.interval = null;
+			}
+		
+		});	
+	}
+		
+	$.fn.rainbow.render = function(obj) {
+	
+			var options = $(obj).data('options');
+			var chars = options.originalText.split('');
+
+			options.iterations++;
+				
+			var newstr = '';
+			var counter = 0;
+			for (var x in chars) {
+			
+				if (chars[x]!=' ') {
+					newstr = newstr + '<span style="color: ' + options.colors[counter] + ';">' + chars[x] + '</span>';
+					counter++;
+				} else {
+					newstr = newstr + ' ';
+				
+				}
+			
+			
+				if (counter >= options.colors.length) {
+					counter = 0;
+				}
+			}
+			$(obj).html(newstr);
+	
+			var pause = (options.iterations % options.colors.length == 0);
+			
+
+
+			if (options.animate) {
+			
+				(
+					function(obj,interval) {
+						console.log("setting interval to " + interval);
+						var options = $(obj).data('options');
+						var i = setTimeout(function() {
+							$.fn.rainbow.shift(obj);
+						},interval);
+						options.interval = i;
+						$(obj).data('options',options);
+					}
+				)(obj,pause?options.pauseLength:options.animateInterval);
+			}	
+
+			
+	}
+	
+	
+	$.fn.rainbow.shift = function(obj) {
+	
+		var options = $(obj).data('options');
+		var color = options.colors.pop();
+		options.colors.unshift(color);
+		$.fn.rainbow.render(obj);
+	
+	}
+	
+})(jQuery);
+
+
+
+
